ae9d0f3f4dfa6b717b2ce7d31ae5a92bff70f9d7
YHR <h_yacine@live.fr>
2022-07-08 16:02:11 +0200

    Shape up navbar

---
+++ src/components/Navigation/Nav.styled.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from "react"
+import styled from "styled-components"
+import logo from "~/logo.svg"
+import { CTA } from "./CTA.styled"
+import { Menu } from "./Menu.styled"
+import { Logo } from "./Logo.styled"
+
+const links = [
+  { name: "Product", href: "/", dropdown: true },
+  { name: "Pricing", href: "/", dropdown: false },
+  { name: "Customers", href: "/", dropdown: false },
+  { name: "Learn", href: "/", dropdown: true },
+]
+
+const Container = styled.nav`
+  margin-top: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
+
+export const Nav: FC = () => (
+  <Container>
+    <Logo href="/" src={logo} title="Logo" />
+    <Menu links={links} />
+    <CTA />
+  </Container>
+)
