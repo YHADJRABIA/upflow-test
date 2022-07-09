240af45d0049810d075bc1a6eec0ce514383c7e4
YHR <h_yacine@live.fr>
2022-07-10 00:11:46 +0200

    Implement all sections on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from "next/head"
-import { Container, Main } from "@/components/sharedstyles"
+import { Container } from "@/components/sharedstyles"
 import { Nav } from "@/components/Navigation/Nav.styled"
 import { Hero } from "@/components/Hero/Hero.styled"
+import { BottomTrial } from "@/components/BottomTrial/BottomTrial.styled"
+import { More } from "@/components/More/More.styled"
+import { MiddleTrial } from "@/components/MiddleTrial/MiddleTrial.styled"
+import { Features } from "@/components/Features/Features.styled"
+import { Product } from "@/components/Product/Product.styled"
 
 export default function Home() {
   return (
@@ -16,7 +21,11 @@ export default function Home() {
       </Head>
       <Nav />
       <Hero />
-      <Main></Main>
+      <Product />
+      <Features />
+      <MiddleTrial />
+      <More />
+      <BottomTrial />
     </Container>
   )
 }
