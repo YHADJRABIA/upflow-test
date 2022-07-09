a930e9fb559891968d0fa478a77dfd9fc8db064e
YHR <h_yacine@live.fr>
2022-07-09 03:33:54 +0200

    Rename `svg` props to fix dev tool errors

--- src/assets/Triangle.tsx
+++ src/assets/Triangle.tsx
@@ -25,9 +25,9 @@ export const Triangle = ({ color, ...rest }: PropTypes) => {
           width="27.9137"
           height="27.9137"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
