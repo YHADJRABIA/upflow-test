5dfe844c93579f41ab4331c752550cf8d010e7b7
YHR <h_yacine@live.fr>
2022-07-11 00:17:39 +0200

    Add `Product` section

--- src/components/Product/Product.styled.tsx
+++ src/components/Product/Product.styled.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react"
 import styled from "styled-components"
+import { Dashboard } from "./Dashboard.styled"
+import { Main } from "./Main.styled"
 
 const Container = styled.section`
   display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
   height: 640px;
-  width: 100%;
+  width: 1200px;
   margin-top: 104px;
   border-radius: 12px;
   box-shadow: 0px 48px 64px rgba(27, 27, 26, 0.02),
@@ -14,4 +18,9 @@ const Container = styled.section`
     0px 0px 0px 1px rgba(27, 27, 26, 0.12);
 `
 
-export const Product: FC = () => <Container></Container>
+export const Product: FC = () => (
+  <Container>
+    <Dashboard />
+    <Main />
+  </Container>
+)
